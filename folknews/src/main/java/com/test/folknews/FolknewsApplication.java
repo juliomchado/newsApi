9129d9ab38243ad28fbc70eaa52de2a19efc1563
Julio Machado <juliocarlos00@hotmail.com>
2021-01-05 17:49:51 -0300

    initial commit

---
+++ folknews/src/main/java/com/test/folknews/FolknewsApplication.java
@@ -0,0 +1,13 @@
+package com.test.folknews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolknewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolknewsApplication.class, args);
+	}
+
+}
